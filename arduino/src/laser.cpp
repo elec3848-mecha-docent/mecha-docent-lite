70489aed257f4b938340e06c64a932bc96f6ffea
Ilo Japar <ignatiusdeloyoladj@gmail.com>
2026-04-24 01:03:53 +0800

    Add laser and servo control classes with basic functionality

---
+++ arduino/src/laser.cpp
@@ -0,0 +1,30 @@
+#include "laser.h"
+
+LaserController::LaserController(uint8_t pin) : laserPin(pin), laserState(false) {}
+
+void LaserController::begin() {
+    pinMode(laserPin, OUTPUT);
+    turnOff();
+}
+
+void LaserController::turnOn() {
+    digitalWrite(laserPin, HIGH);
+    laserState = true;
+}
+
+void LaserController::turnOff() {
+    digitalWrite(laserPin, LOW);
+    laserState = false;
+}
+
+void LaserController::toggle() {
+    if (laserState) {
+        turnOff();
+    } else {
+        turnOn();
+    }
+}
+
+bool LaserController::isOn() const {
+    return laserState;
+}
